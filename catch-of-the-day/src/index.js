23fcc29a8556a3e5b7a6a00fcb08305c72aa9bd0
Nathaniel Baulch-Jones <xdnatty@gmail.com>
2018-08-14 01:12:15 +0100

    Good practice - components in folders

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import {render} from 'react-dom'
+import {render} from 'react-dom';
+import StorePicker from './components/StorePicker';
 
-class StorePicker extends React.Component {
-    render() {
-        return <p>Hello!</p>
-    }
-}
 
 render(<StorePicker />, document.querySelector('#main'))
